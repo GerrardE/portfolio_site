84da3df4bf20f6d24c79c821ed7467a30c1928c5
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2021-11-01 22:17:25 +0100

    Fix issues with seo

--- client/src/ui/pages/Category.jsx
+++ client/src/ui/pages/Category.jsx
@@ -45,9 +45,9 @@ const Category = (props) => {
   const seo = {
     description: "Articles, tips and tricks to help you on your path to becoming a world-class Software Engineer.", 
     url: `${process.env.REACT_APP_BASE_URL}/blog`, 
-    title: "Ezeugwa Gerrard", 
+    title: "Ezeugwa Gerrard | Category", 
     image: "https://res.cloudinary.com/dz9mitahp/image/upload/v1635609775/small_ezeugwagerrard_f0a822c23e.jpg?7751481.100000024",
-    keywords: "Ezeugwa Gerrard's Personal Blog"
+    keywords: "Ezeugwa Gerrard's, Personal Blog"
   }
 
   return (
