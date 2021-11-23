557cd861f9d464941851796bfdd925268b21939d
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2021-11-23 13:14:08 +0100

    update postview logic

--- server/api/postview/services/postview.js
+++ server/api/postview/services/postview.js
@@ -1,8 +1,17 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  countViews(params) {
+    return strapi.query("postview").model.fetchAll({
+        columns: ["id", "post", "viewIp"],
+        withRelated: [{
+            "post": qb => {qb.columns("id")},
+        }]
+    });
+  },
+};
